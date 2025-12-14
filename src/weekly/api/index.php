707114bc449965794cbd6e5b3ab43f2223bf01f9
Mohamed Abdulla Aldoseri <mohammedaldosri2003.com>
2025-12-15 00:03:44 +0300

    Update

--- src/weekly/api/index.php
+++ src/weekly/api/index.php
@@ -46,6 +46,7 @@
 // Allow specific headers (Content-Type, Authorization)
 // Allow from any origin (adjust in production)
 session_start();
+$_SESSION['user_initialized'] = true;
 
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
